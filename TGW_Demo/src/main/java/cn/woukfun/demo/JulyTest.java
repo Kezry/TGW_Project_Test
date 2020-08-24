b0e700d39333c6d8ade52cd390ea638c14490e10
July <July@itcast.cn>
2020-08-24 20:25:55 +0800

    test_test_test

--- TGW_Demo/src/main/java/cn/woukfun/demo/JulyTest.java
+++ TGW_Demo/src/main/java/cn/woukfun/demo/JulyTest.java
@@ -3,5 +3,7 @@ package cn.woukfun.demo;
 public class JulyTest {
     public static void main(String[] args) {
         System.out.println("这是July写的一个git项目测试文件!");
+
+        System.out.println("提交到自己的仓库");
     }
 }
